5cfc30b4a297b1c5f146272fbb08035fca4cfacf
Heather Swank <hdamoth@gmail.com>
2025-10-19 21:07:07 -0400

    Added error handling and fixed syntax error

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -6,25 +6,28 @@ print('Hello! What is your name?')
 myName = input()
 
 number = random.randint(1,20)
-print('Well, ' + myName + ', I am thinking of a number between 1 nad 20.')
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
 for guessesTaken in range(6):
     print('Take a guess.')
     guess = input()
-    guess = int(guess)
+    try:
+        guess = int(guess)
+    except:
+        print('Please enter a number.')
+    else:
+        if guess < number:
+            print('Your guess is too low.')
 
-    if guess < number:
-        print('Your guess is too low.')
+        if guess > number:
+            print('Your guess is too high.')
 
-    if guess > number:
-        print('Your guess is too high.')
-
-    if guess == number:
-        break
+        if guess == number:
+            break
 
 if guess == number:
     guessesTaken = str(guessesTaken + 1)
-    print('Good job, ' + myName + ('! You guessed my number in ' + guessesTaken + ' guesses!')
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
 
 if guess != number:
     number = str(number)
